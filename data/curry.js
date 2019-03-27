467acb285f09c76327ab6a03c9222799254506b3
elanmarae <elan.ward@uky.edu>
2019-03-26 18:29:38 -0700

    .
    .

---
+++ data/curry.js
@@ -0,0 +1,9 @@
+var curry = { "type": "FeatureCollection", "name": "coco_curry", "features": [
+{ "type": "Feature", "properties": { "NAME_EN": "CoCo Ichibanya Okinawa South Inter" }, "geometry": { "type": "Point", "coordinates": [ 127.791509, 26.335858 ] } },
+{ "type": "Feature", "properties": { "NAME_EN": "CoCo Ichibanya Okinawa Misato Store" }, "geometry": { "type": "Point", "coordinates": [ 127.818244, 26.350382 ] } },
+{ "type": "Feature", "properties": { "NAME_EN": "CoCo Ichibanya Urasoe Barclays Court Store" }, "geometry": { "type": "Point", "coordinates": [ 127.738127, 26.252384 ] } },
+{ "type": "Feature", "properties": { "NAME_EN": "CoCo Ichibanya Okinawa Chatan Kokutai Douri Store" }, "geometry": { "type": "Point", "coordinates": [ 127.76637, 26.326371 ] } },
+{ "type": "Feature", "properties": { "NAME_EN": "CoCo Ichibanya Naha Ameku Store" }, "geometry": { "type": "Point", "coordinates": [ 127.693007, 26.233354 ] } },
+{ "type": "Feature", "properties": { "NAME_EN": "CoCo Ichibanya Itoman Shiohira Store" }, "geometry": { "type": "Point", "coordinates": [ 127.672444, 26.149366 ] } }
+]
+}
